0f9d05712bfcbd805191540146fed53be8d62146
Pedro Silva <pedro.b.silva3@ext.leroymerlin.pt>
2026-04-15 17:57:42 +0100

    fix: remove debug string

--- frontend/components/DigitalProductDetails.vue
+++ frontend/components/DigitalProductDetails.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="text-sm">
-    Hello
     <div class="flex items-center text-blue-400 mb-1">
       <svg class="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4"></path>
